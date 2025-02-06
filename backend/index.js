96e0733b138178a3a57bb968417303365957c384
Arnab <arnabk2001@gmail.com>
2025-02-06 19:23:19 +0530

    sample prototype is ready

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL); // Allow any origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+  res.header('Access-Control-Allow-Credentials', true); // Allow credentials
   next();
 });
 
