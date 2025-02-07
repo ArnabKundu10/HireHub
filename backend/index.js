9fe36299ce1dd7b21da67336454b681e3cc785e0
Arnab <arnabk2001@gmail.com>
2025-02-07 09:53:06 +0530

    Sample prototype change

--- backend/index.js
+++ backend/index.js
@@ -11,33 +11,28 @@ dotenv.config();
 connectDB();
 
 const app = express();
-// app.use(express.json());
-// app.use(cookieParser());
-// const allowedOrigins = [
-//   "https://hire-hub-lwwa.vercel.app", // Frontend URL
-//   "https://hire-hub-zeta-eight.vercel.app" // Backend URL
-// ];
-
+const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "*",
-    credentials: true, // Ensure credentials are allowed
-    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-    // allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-    // exposedHeaders: ["Set-Cookie"] // Expose Set-Cookie header
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
   })
 );
 
-app.use(express.json()); // Parse JSON requests
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // Parse cookies
 
 // Routes
+app.get("/",(req,res)=>{
+  res.status(201).json("Server is okay");
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
 
 // Error Handling Middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
